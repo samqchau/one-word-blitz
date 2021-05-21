d8af1290bf98cc6e261ff1a172177667eb75da8b
samqchau <67344952+samqchau@users.noreply.github.com>
2021-05-20 21:13:20 -0700

    Word Display Component Update Component was updated to use a function that removes all punctuation from the single displayed word

--- src/components/WordDisplay.jsx
+++ src/components/WordDisplay.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import '../styles/wordDisplay.css'
+import React, { useState, useEffect } from 'react';
+import '../styles/wordDisplay.css';
+import { cleanWord } from '../utils/helperFunctions';
 
-const WordDisplay = ({passage, index}) => {
-    return (
-        <div className='wordDisplay'>
-            <h6>Word Display</h6>
-            {passage[index]}
-            {index}
-        </div>
-    )
-}
+const WordDisplay = ({ passage, index }) => {
+  let [word, setWord] = useState(cleanWord(passage[index]));
+  useEffect(() => {
+    setWord(cleanWord(passage[index]));
+  }, [index, passage]);
+  return (
+    <div className='wordDisplay'>
+      <h6>Word Display</h6>
+      {index} {word}
+    </div>
+  );
+};
 
-export default WordDisplay
+export default WordDisplay;
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,8 +1,11 @@
-export function parsePassage (passage) {
-    //returns parsed passage - we want an [] with all the words
-    return passage.split(' ');
+export function parsePassage(passage) {
+  //returns parsed passage - we want an [] with all the words
+  return passage.split(' ');
 }
 
-export function cleanWords(word){
-    //removes all the extra stuff like apostrophes, commas, periods, and could potentially return how much time to add to the interval for pauses like commas and periods.
+export function cleanWord(word) {
+  //removes all the extra stuff like apostrophes, commas, periods, and could potentially return how much time to add to the interval for pauses like commas and periods.
+  let cleanWord = word.replace(/\./g, '');
+  cleanWord = cleanWord.replace(/,/g, '');
+  return cleanWord;
 }
