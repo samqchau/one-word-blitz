a4e91cfb92ccf04b2201cae5d8e6780f352b5280
samqchau <67344952+samqchau@users.noreply.github.com>
2021-05-20 22:41:18 -0700

    Passage Display Component Updated Added stylesheet path and added classes using the BEM naming convention

--- src/components/PassageDisplay.jsx
+++ src/components/PassageDisplay.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React from 'react';
+import '../styles/passageDisplay.css';
 
 const highlightColor = 'rgb(255, 249, 179)';
 
-const PassageDisplay = ({passage, index, title}) => {
-    return (
-        <div className='passageDisplay'>
-            <h2>{title}</h2>
-            {passage.map((word, i) => 
-            <div style={{display: 'inline-flex', background: i===index ? highlightColor : 'none'}} key={i}>                
-                <span style={{width: '3px', background: (i-index === 1 || i-index === 0) ? highlightColor : 'none'}}/>
-                <div key={i} style={{}}>{word}</div>
-            </div>
-            )}
+const PassageDisplay = ({ passage, index, title }) => {
+  return (
+    <div className='passageDisplay'>
+      <div className='passageDisplay-header'>
+        <h2 className='passageDisplay-header-title'>{title}</h2>
+        <p className='passageDisplay-header-author'>Written by : Samuel Chau</p>
+        <p className='passageDisplay-header-date'>On 5/20/2021</p>
+      </div>
+      <br />
+      {passage.map((word, i) => (
+        <div
+          style={{
+            display: 'inline-flex',
+            background: i === index ? highlightColor : 'none',
+            paddingRight: '3px',
+            paddingLeft: '3px',
+          }}
+          key={i}
+        >
+          {word}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default PassageDisplay
+export default PassageDisplay;
