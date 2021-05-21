8074cdb0edb0354b52432447d419086a8af953f8
samqchau <67344952+samqchau@users.noreply.github.com>
2021-05-20 21:46:45 -0700

    Short Stories Folder Added data folder to hold static short stories

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import {ListGroup, ListGroupItem} from 'react-bootstrap';
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 import '../styles/sidebar.css';
 import PassageListing from '../components/PassageListing.jsx';
+import { buildingTheButternut } from '../data/passages/100wordStories.js';
+import { bewareTheBeast } from '../data/passages/200wordStories.js';
 
-const Sidebar = ({changePassageTo, changeTitleTo, resetIndex}) => {
-    return (
-        <div className='sidebar'>
-            <ListGroup className='sidebar-passageList'>
-                What's it like to read x00 words per minute?
-                <PassageListing changePassageTo={changePassageTo} changeTitleTo={changeTitleTo} title='100 wpm' content='This is a one hundred word short story' resetIndex={resetIndex}/>
-                <PassageListing changePassageTo={changePassageTo} changeTitleTo={changeTitleTo} title='200 wpm' content='This is a one hundred word short story' resetIndex={resetIndex}/>
-                <PassageListing changePassageTo={changePassageTo} changeTitleTo={changeTitleTo} title='300 wpm' content='This is a one hundred word short story' resetIndex={resetIndex}/>
-                <PassageListing changePassageTo={changePassageTo} changeTitleTo={changeTitleTo} title='400 wpm' content='This is a one hundred word short story' resetIndex={resetIndex}/>
-                <PassageListing changePassageTo={changePassageTo} changeTitleTo={changeTitleTo} title='500 wpm' content='This is a one hundred word short story' resetIndex={resetIndex}/>
-            </ListGroup>
-        </div>
-    )
-}
+const Sidebar = ({ changePassageTo, changeTitleTo, resetIndex }) => {
+  return (
+    <div className='sidebar'>
+      <ListGroup className='sidebar-passageList'>
+        What's it like to read x00 words per minute?
+        <PassageListing
+          changePassageTo={changePassageTo}
+          changeTitleTo={changeTitleTo}
+          title={buildingTheButternut.title}
+          content={buildingTheButternut.content}
+          resetIndex={resetIndex}
+        />
+        <PassageListing
+          changePassageTo={changePassageTo}
+          changeTitleTo={changeTitleTo}
+          title={bewareTheBeast.title}
+          content={bewareTheBeast.content}
+          resetIndex={resetIndex}
+        />
+        <PassageListing
+          changePassageTo={changePassageTo}
+          changeTitleTo={changeTitleTo}
+          title='300 wpm'
+          content='This is a one hundred word short story'
+          resetIndex={resetIndex}
+        />
+        <PassageListing
+          changePassageTo={changePassageTo}
+          changeTitleTo={changeTitleTo}
+          title='400 wpm'
+          content='This is a one hundred word short story'
+          resetIndex={resetIndex}
+        />
+        <PassageListing
+          changePassageTo={changePassageTo}
+          changeTitleTo={changeTitleTo}
+          title='500 wpm'
+          content='This is a one hundred word short story'
+          resetIndex={resetIndex}
+        />
+      </ListGroup>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ src/data/passages/100wordStories.js
@@ -0,0 +1,5 @@
+export let buildingTheButternut = {
+  title: 'Building The Butternut',
+  content:
+    'It didn’t seem right, but we set the clocks and got the children up at midnight to go out and yell at the squash. A time-tested deterrent, it didn’t harm the vegetables, but the negative voice lodged in the prickly vine, alarming the squirrels like the sentry crows, who swooped down in a rage to chase them away. When we ran out of children we tried pre-recorded messages, but the squirrels severed the connection and dragged off the squash. Now, we summon our inner children, and the squash grows large. But we have to be quiet because of the neighbors.',
+};
