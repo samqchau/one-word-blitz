6d47de6b49228f89637bc58b334ec681a0d36ee3
samqchau <67344952+samqchau@users.noreply.github.com>
2021-05-25 15:25:18 -0700

    Word Controls Component Updated -Word Control Component now plays and stops correctly -Word Control Component allows users to set how many words per minute they would like the player to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,60 @@
-import React, {useState, useEffect} from 'react';
-import {Container} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import './styles/app.css';
-import Sidebar from './components/Sidebar.jsx'
+import Sidebar from './components/Sidebar.jsx';
 import Homepage from './screens/Homepage.jsx';
-import {parsePassage} from './utils/helperFunctions.js'
+import { parsePassage } from './utils/helperFunctions.js';
 
 const App = () => {
-    const [passage, setPassage] = useState(parsePassage('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'));
-    const [index, setIndex] = useState(0);
-    const [title, setTitle] = useState('Lorem Ipsum');
-
-    const changePassageTo = (newPassage) => {
-        setPassage(newPassage);
-    }
-    const changeTitleTo = (title) => {
-        setTitle(title);
-    }
-    const incrementIndex = () => {
-        setIndex(idx => {
-            return idx >= passage.length-1 ? idx : idx+1
-        });
-    }
-
-    const decrementIndex = () => {
-        setIndex(idx => {
-            return idx === 0 ? 0 : idx-1
-        })
-    }
-    const resetIndex = () => {
-        setIndex(0);
-    }
-
-    return (
-        <Container className='app d-flex'>
-            <Sidebar changePassageTo={changePassageTo} changeTitleTo={changeTitleTo} resetIndex={resetIndex}/>
-            <Container className='app-main'>
-                <Homepage passage={passage} changePassageTo={changePassageTo}
-                index={index}
-                incrementIndex={incrementIndex}
-                decrementIndex={decrementIndex}
-                resetIndex={resetIndex}
-                title={title}
-                />
-            </Container>
-        </Container>
+  const [passage, setPassage] = useState(
+    parsePassage(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     )
-}
+  );
+  const [index, setIndex] = useState(0);
+  const [title, setTitle] = useState('Lorem Ipsum');
 
-export default App
+  const changePassageTo = (newPassage) => {
+    setPassage(newPassage);
+  };
+  const changeTitleTo = (title) => {
+    setTitle(title);
+  };
+  const incrementIndex = () => {
+    setIndex((idx) => {
+      return idx >= passage.length - 1 ? idx : idx + 1;
+    });
+  };
+
+  const decrementIndex = () => {
+    setIndex((idx) => {
+      return idx === 0 ? 0 : idx - 1;
+    });
+  };
+  const resetIndex = () => {
+    setIndex(0);
+  };
+
+  return (
+    <Container className='app d-flex'>
+      <Container className='app-main'>
+        <Homepage
+          passage={passage}
+          changePassageTo={changePassageTo}
+          index={index}
+          incrementIndex={incrementIndex}
+          decrementIndex={decrementIndex}
+          resetIndex={resetIndex}
+          title={title}
+        />
+      </Container>
+      <Sidebar
+        changePassageTo={changePassageTo}
+        changeTitleTo={changeTitleTo}
+        resetIndex={resetIndex}
+      />
+    </Container>
+  );
+};
+
+export default App;
--- src/components/WordControls.jsx
+++ src/components/WordControls.jsx
@@ -1,17 +1,93 @@
-import React from 'react'
-import {Button} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Button, FormControl, InputGroup, Container } from 'react-bootstrap';
 import '../styles/wordControls.css';
 
-const WordControls = ({incrementIndex, decrementIndex, resetIndex}) => {
-    return (
-        <div className='wordControls'>
-            <h2>Word Controls</h2>
-            <Button variant='info' onClick={decrementIndex}>{'<'}</Button>
-            <Button onClick={resetIndex}>To The beginning</Button>
-            <Button>Stop/Play</Button>
-            <Button variant='info' onClick={incrementIndex}>{'>'}</Button>
-        </div>
-    )
-}
+const WordControls = ({ incrementIndex, decrementIndex, resetIndex }) => {
+  let [wpm, setWpm] = useState(200);
+  let [playing, setPlaying] = useState(false);
 
-export default WordControls
+  let togglePlaying = () => {
+    setPlaying(!playing);
+  };
+
+  //Params (Desired WPM)
+  //Returns (miliseconds Per Word)
+  let wordEveryMiliseconds = (wpm) => {
+    return 60000 / wpm;
+  };
+
+  useEffect(() => {
+    let interval;
+    if (playing) {
+      interval = setInterval(() => {
+        incrementIndex();
+      }, wordEveryMiliseconds(wpm));
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [playing, wpm, incrementIndex]);
+
+  return (
+    <div className='wordControls'>
+      <h2>Word Controls</h2>
+      <Container>
+        <Button
+          variant='dark'
+          onClick={() => {
+            decrementIndex();
+            setPlaying(false);
+          }}
+        >
+          {'<'}
+        </Button>
+        <Button
+          variant='dark'
+          onClick={() => {
+            resetIndex();
+            setPlaying(false);
+          }}
+        >
+          To The beginning
+        </Button>
+        <Button variant='dark' onClick={togglePlaying}>
+          Stop/Play
+        </Button>
+        <Button
+          variant='dark'
+          onClick={() => {
+            incrementIndex();
+            setPlaying(false);
+          }}
+        >
+          {'>'}
+        </Button>
+        <div className='wordControl-inputGroup-container'>
+          <InputGroup className='mb-3 wordControl-inputGroup'>
+            <InputGroup.Prepend>
+              <InputGroup.Text id='basic-addon1'>WPM</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              className='wordControls-wpmInput-formControl'
+              as='select'
+              placeholder='Words Per Minute'
+              value={wpm}
+              onChange={(e) => {
+                setPlaying(false);
+                setWpm(e.target.value);
+              }}
+            >
+              {[...Array(18).keys()].map((x) => (
+                <option key={x + 1} value={(x + 1) * 25 + 100}>
+                  {(x + 1) * 25 + 100}
+                </option>
+              ))}
+            </FormControl>
+          </InputGroup>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default WordControls;
